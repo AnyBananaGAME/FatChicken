29aeb04a9e6baa26471b44963857baabdfdbafa5
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-08-28 21:25:28 +0300

    Finish Balance command

--- src/commands/Economy/Balance.js
+++ src/commands/Economy/Balance.js
@@ -10,7 +10,54 @@ module.exports = {
   aliases: ["balance"],
   description: "Check your balance",
   execute: async(message, client, args) => {
+    const id = message.author.id;
  
+
+
+    const Mentioned = message.mentions.members.first;
+    if(Mentioned){
+        const id = Mentioned.id;
+
+        let money = db.fetch(`money=${id}`);
+
+        if(money === null){
+            const NotStartedEmbed = new MessageEmbed()
+            .setColor(config.color)
+            .setDescription(`Greetings! ${message.author}\n\nWe've noticed that you are new!\nPlease start using !start, to get a short tutorial about this!\nThank you! -FatOtter`)
+        message.channel.send({embeds: [NotStartedEmbed]})
+        }
+
+        const MentionedBalanceEmbed = new MessageEmbed()
+        .setColor(config.color)
+        .setDescription(`Balance of ${Mentioned.user}\n\nBalance: ${money}`)
+        message.channel.send({embeds: [MentionedBalanceEmbed]})
+    }else {
+        const id = message.author.id;
+
+        let money = db.fetch(`money=${id}`);
+
+        if(money === null){
+            const NotStartedEmbed = new MessageEmbed()
+            .setColor(config.color)
+            .setDescription(`Greetings! ${message.author}\n\nWe've noticed that you are new!\nPlease start using !start, to get a short tutorial about this!\nThank you! -FatOtter`)
+        message.channel.send({embeds: [NotStartedEmbed]})
+        }
+        const MentionedBalanceEmbed = new MessageEmbed()
+        .setColor(config.color)
+        .setDescription(`Balance of ${message.author}\n\nBalance: ${money}`)
+        message.channel.send({embeds: [MentionedBalanceEmbed]})
+        message.channel.send({embeds: [MentionedBalanceEmbed]})
+    
+       }
+        
+
+
+
+
+
+
+
+
 
 
 
