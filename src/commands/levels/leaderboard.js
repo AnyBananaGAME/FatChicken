8e63a3fe956f38032dc951ac3f3285b78ac67b6b
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-08-28 20:55:34 +0300

    top levels leaderboard

---
+++ src/commands/levels/leaderboard.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const config = require('../../../resources/configs/config.json')
+
+const db = require('quick.db')
+module.exports = {
+    name: "lb",
+    description: "Leaderboard for levels",
+    execute: async (message, client, args) => {
+
+      let level = db.all().filter(data => data.ID.startsWith(`level=`)).sort((a, b) => b.data - a.data)
+      var finalLb = "";
+      for (var i in level) {
+        finalLb += `**${level.indexOf(level[i])+1}. ${client.users.cache.get(level[i].ID.split('=')[1]) ? client.users.cache.get(level[i].ID.split('=')[1]).tag : "Unknown User#0000"}** - ${level[i].data}\n`;
+      }
+
+      const embed = new Discord.MessageEmbed()
+      .setAuthor(`Levels Leaderboard!`, message.guild.iconURL())
+      .setColor("#7289da")
+      .setDescription(`${finalLb}`)
+      .setFooter(client.user.tag, client.user.displayAvatarURL())
+      .setTimestamp()
+      message.channel.send({embeds: [embed]});
+
+
+    }
+}
