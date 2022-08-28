c9e1e7fa7c4eefbe824d660e6eaf7d0bcd46d392
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-08-28 21:13:53 +0300

    Balance Commands

---
+++ src/commands/Economy/Balance.js
@@ -0,0 +1,20 @@
+const chalk = require("chalk")
+const Fs = require("fs");
+const config = require('../../../resources/configs/config.json')
+const nps = require('../../../resources/configs/nps.json')
+
+const db = require("quick.db");
+const { MessageEmbed } = require("discord.js")
+module.exports = {
+  name: "bal",
+  aliases: ["balance"],
+  description: "Check your balance",
+  execute: async(message, client, args) => {
+ 
+
+
+
+
+    }
+}
+ 
