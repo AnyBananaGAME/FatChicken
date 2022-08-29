3378d45d82bdf28e0e46980d6209ba74a2e01cee
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-08-29 13:19:43 +0300

    uptime command

---
+++ src/commands/Public/uptime.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require("discord.js");
+const Fs = require("fs");
+const config = require('../../../resources/configs/config.json')
+module.exports = {
+  name: "uptime",
+  aliases: ["up"],
+  description: "Check the server's information",
+  execute: async(message, client, args) => {
+ 
+
+
+
+
+
+
+    }
+}
+ 
