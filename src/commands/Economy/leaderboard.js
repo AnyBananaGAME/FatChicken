e5d4779d6b93214af339340f26f777967d1be822
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-08-29 13:15:24 +0300

    remove footer cause it's bad

---
+++ src/commands/Economy/leaderboard.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const config = require('../../../resources/configs/config.json')
+
+const db = require('quick.db')
+module.exports = {
+    name: "lb",
+    description: "Leaderboard for money",
+    execute: async (message, client, args) => {
+
+      let money = db.all().filter(data => data.ID.startsWith(`money=`)).sort((a, b) => b.data - a.data)
+      var finalLb = "";
+      for (var i in money) {
+        finalLb += `**${money.indexOf(money[i])+1}. ${client.users.cache.get(money[i].ID.split('=')[1]) ? client.users.cache.get(money[i].ID.split('=')[1]).tag : "Unknown User#0000"}** - ${money[i].data}\n`;
+      }
+
+      const embed = new Discord.MessageEmbed()
+      .setAuthor(`Money Leaderboard!`, message.guild.iconURL())
+      .setColor(config.color)
+      .setDescription(`${finalLb}`)
+      .setFooter(client.user.tag, client.user.displayAvatarURL())
+      .setTimestamp()
+      message.channel.send({embeds: [embed]});
+
+
+    }
+}
