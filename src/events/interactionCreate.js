a311d3befd43c2bdd2d8004526365626e4268f4d
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-09-02 15:25:40 +0300

    InteractionCreate

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,24 @@
+const chalk = require('chalk')
+const Discord = require('discord.js');
+const config = require("../../resources/configs/config.json")
+const Fs = require("fs");
+
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction, client) {
+
+
+        if (!interaction.isCommand()) return;
+
+        const slashcommand = client.slashcommands.get(interaction.commandName);
+
+        if (!slashcommand) return;
+        try {
+            await slashcommand.execute(interaction, client);
+        } catch (error) {
+            console.log(chalk.red(`!!!`) + chalk.gray(` ${error}`))
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        
+    }
+    }
+}
