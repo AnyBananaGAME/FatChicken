b12b125032ea4a2da560f7e155e998f43ec94649
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-09-02 13:55:05 +0300

    finish suggest command

--- src/commands/Public/suggest.js
+++ src/commands/Public/suggest.js
@@ -3,12 +3,40 @@ const Fs = require("fs");
 const config = require('../../../resources/configs/config.json')
 module.exports = {
   name: "suggest",
-  aliases: ["sgs"],
   description: "Suggest a fewture for the bot, or a change for discord guild",
   execute: async(message, client, args) => {
  
+
     if(message.channel.id !== "1014482651014512691"){
-        message.channel.send({content: "This command can only be executed in bot commands channel", ephemeral: true})
+    const msg = await message.channel.send(`You can only use this command in Bot-Commands channel! (3)`)
+        await msg.edit(`You can only use this command in Bot-Commands channel! (3)`)
+
+        await msg.edit(`You can only use this command in Bot-Commands channel! (2)`)
+        await msg.edit(`You can only use this command in Bot-Commands channel! (2)`)
+
+        await msg.edit(`You can only use this command in Bot-Commands channel! (1)`)
+        await msg.delete()
+    
+    } else {
+        if(!args[0]){
+            message.reply('(.suggest suggestion)')
+            return;
+        }
+        const suggestion = message.content.slice(8).trim();
+        let SuggestionEmbed = new MessageEmbed()
+        .setColor(config.colour)
+        .addField('Suggested by', `${message.author.tag}`)
+        .addField('Suggestion',`${suggestion}`) 
+        .setTimestamp()
+        .setFooter(message.author.username, message.author.displayAvatarURL())
+        
+    client.channels.cache.get('1013777232990310461').send({ embeds: [SuggestionEmbed]}).then((mesg) => {
+        mesg.react('👍')
+        mesg.react('👎')
+    })
+
+
+
     }
 
     }
