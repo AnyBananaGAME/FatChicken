c4479b8c302b0b6d233e494e71dcb493f7233a33
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-09-03 21:36:27 +0300

    ok i stupid

--- src/commands/Owner/generate.js
+++ src/commands/Owner/generate.js
@@ -11,10 +11,12 @@ module.exports = {
     
         if(message.author.id != config.owner) return;
 
-        const filecontents = 'const chalk = require("chalk")\nconst fs = require("fs")\nconst config = require("../../../resources/configs/config.json")\nmodule.exports = {\n  name: "' + args[1]+'",\n  aliases: ["'+args[1]+'"],\n  description: "This command skeleton was generated",\n  execute: async(message, client, args) => {\n    try{\n\n\n\n    }catch(error){\n      const channel = client.channels.cache.get("1015523931274354770")\n      const ErrorEmbed = new MessageEmbed()\n          .setColor(config.color)\n          .setDescription(`------------Date------------\n **${Date()}**\n\n------------Error------------\n **${error}**\n\n------------Caused by------------\n**${message.author.tag} ( ${message.author.id} **)`)\n       await channel.send({embeds: [ErrorEmbed]})\n }}} '
-        fs.writeFile(`${args[0]}.js`, `${filecontents}`, function (err) {
+        const filecontents = 'const chalk = require("chalk")\nconst fs = require("fs")\nconst config = require("../../../resources/configs/config.json")\nmodule.exports = {\n  name: "' + args[1]+'",\n  aliases: ["'+args[1]+'"],\n  description: "This command skeleton was generated",\n  execute: async(message, client, args) => {\n    try{\n\n\n\n    }catch(error){\n      const channel = client.channels.cache.get("1015523931274354770")\n      const ErrorEmbed = new MessageEmbed()\n          .setColor(config.color)\n          .setDescription(`------------Date------------~n **${Date()}**~n~n------------Error------------~n **${error}**~n~n------------Caused by------------~n**${message.author.tag} ( ${message.author.id} **)`\n       await channel.send({embeds: [ErrorEmbed]})\n }}} '
+        const stringed = filecontents.toString()
+        const replaced = stringed.replaceAll('~', "\ ");
+        fs.writeFile(`${args[0]}.js`, `${replaced}`, function (err) {
             if (err) return console.log(err);
-            console.log(`${args[0]} >>> ${filecontents}`);
+            console.log(`${args[0]} >>> ${replaced}`);
           });
 
 
