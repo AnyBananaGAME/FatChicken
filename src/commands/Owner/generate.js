0ae4b45f8f0fc3bd134f8cd3d2280ac7b19d0095
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-09-03 21:29:04 +0300

    fix some stof with generat command

--- src/commands/Owner/generate.js
+++ src/commands/Owner/generate.js
@@ -14,7 +14,7 @@ module.exports = {
         const filecontents = 'const chalk = require("chalk")\nconst fs = require("fs")\nconst config = require("../../../resources/configs/config.json")\nmodule.exports = {\n  name: "' + args[1]+'",\n  aliases: ["'+args[1]+'"],\n  description: "This command skeleton was generated",\n  execute: async(message, client, args) => {\n    try{\n\n\n\n    }catch(error){\n      const channel = client.channels.cache.get("1015523931274354770")\n      const ErrorEmbed = new MessageEmbed()\n          .setColor(config.color)\n          .setDescription(`------------Date------------\n **${Date()}**\n\n------------Error------------\n **${error}**\n\n------------Caused by------------\n**${message.author.tag} ( ${message.author.id} **)`)\n       await channel.send({embeds: [ErrorEmbed]})\n }}} '
         fs.writeFile(`${args[0]}.js`, `${filecontents}`, function (err) {
             if (err) return console.log(err);
-            console.log(`${args[0]} >>> ${args[1]}`);
+            console.log(`${args[0]} >>> ${filecontents}`);
           });
 
 
