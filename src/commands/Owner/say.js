dffaa0a3134c24f86a8ac67b0ad60bc3c348a925
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-09-03 10:56:53 +0300

    Rewrite say command

--- src/commands/Owner/say.js
+++ src/commands/Owner/say.js
@@ -6,7 +6,7 @@ module.exports = {
   aliases: ["send"],
   description: "Send a message as a bot",
   execute: async(message, client, args) => {
- 
+    try{
     
 
         message.delete()
@@ -14,6 +14,16 @@ module.exports = {
         if (message.author.bot) return;
         const SayMessage = message.content.slice(4).trim();
         message.channel.send("" + SayMessage + "" )
+
+    }catch(error){
+      const channel = client.channels.cache.get('1015523931274354770')
+      const ErrorEmbed = new MessageEmbed()
+          .setColor(config.color)
+          .setDescription(`------------Date------------\n **${Date()}**\n\n------------Error------------\n **${error}**\n\n------------Caused by------------\n**${message.author.tag} ( ${message.author.id} **)`)
+      await channel.send({embeds: [ErrorEmbed]})
+
+    }
+
 
     }
 }
