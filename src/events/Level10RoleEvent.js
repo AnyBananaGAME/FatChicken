775077456b1899bdc631e7f9f089baeb1942319c
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-09-03 20:25:09 +0300

    fix some stuff with level 10 roles

--- src/events/Level10RoleEvent.js
+++ src/events/Level10RoleEvent.js
@@ -5,6 +5,7 @@ const { MessageEmbed } = require("discord.js")
 module.exports = {
     name: 'messageCreate',
     async execute(message, client) {
+      try{
       if(message.author.bot) return;
 
       const id = message.author.id;
@@ -16,13 +17,20 @@ module.exports = {
 
 
     if(level >= "10"){
+      
         const guild  = message.channel.guild;
         var role = guild.roles.cache.find(r => r.name === 'Bamboo');
-        message.authorroles.add(role);
+        if(message.member.roles.cache.find(r => r.name === "Bamboo")) {
+          return;
+      }
+      if (role) message.guild.members.cache.get(message.author.id).roles.add(role);
         message.channel.send(`Congrats ${message.author}, you just received a ROLE! for reaching level 10`)
-    }
-
+    }  
+    
+    
+  }catch(error) {
+    await  message.channel.send(`${error}`)
+  }
         
-
-    }
+ }
 }
