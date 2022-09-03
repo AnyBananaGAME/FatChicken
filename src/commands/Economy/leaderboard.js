75a87da3ef8505cede985baff2cfbc2ac27c1837
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-09-03 10:55:36 +0300

    Rewite leaderboard command

--- src/commands/Economy/leaderboard.js
+++ src/commands/Economy/leaderboard.js
@@ -6,6 +6,7 @@ module.exports = {
     name: "lb",
     description: "Leaderboard for money",
     execute: async (message, client, args) => {
+      try{
 
       let money = db.all().filter(data => data.ID.startsWith(`money=`)).sort((a, b) => b.data - a.data)
       var finalLb = "";
@@ -19,6 +20,18 @@ module.exports = {
       .setDescription(`\n${finalLb}`)
       .setTimestamp()
       message.channel.send({embeds: [embed]});
+
+
+
+    }catch(error){
+      const channel = client.channels.cache.get('1015523931274354770')
+      const ErrorEmbed = new MessageEmbed()
+          .setColor(config.color)
+          .setDescription(`------------Date------------\n **${Date()}**\n\n------------Error------------\n **${error}**\n\n------------Caused by------------\n**${message.author.tag} ( ${message.author.id} **)`)
+      await channel.send({embeds: [ErrorEmbed]})
+
+
+    }
 
 
     }
